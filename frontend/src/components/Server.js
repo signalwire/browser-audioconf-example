45e9367c73e777612d87d1ecab21eee85171fd91
Daniele Di Sarli <danieleds0@gmail.com>
2021-10-22 16:33:08 +0200

    comments

--- frontend/src/components/Server.js
+++ frontend/src/components/Server.js
@@ -18,25 +18,3 @@ export async function getToken(user, room) {
   });
   return response.data.token;
 }
-
-/**
- * This function obtains a list of existing rooms from our own server.
- * It will be called by the UI to get the list of available rooms.
- * 
- * @returns an array of rooms, each one being an object such as
- * 
- *     {
- *       "id": "1b3f7a21-3191-1175-ae15-30c558a5afbc",
- *       "name": "Roomname1",
- *       "display_name": "Roomname1",
- *       "members": [
- *         { "name": "daniel" },
- *         { "name": "john" },
- *       ]
- *     }
- */
-export async function getRooms() {
-  const response = await axios.get(`${url}/roomsAndParticipants?timestamp=${new Date().getTime()}`);
-  const rooms = response.data;
-  return rooms
-}
